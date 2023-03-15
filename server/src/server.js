5fc391bd540cde88d2ec738d492ef400f263d35a
Jia-Yao Zhao <63852433+zjy040525@users.noreply.github.com>
2023-03-15 10:47:21 +0800

    feat(logger): added local save logs

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,21 @@
-const cors = require('cors')
 const express = require('express')
-const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const fs = require('fs')
+const path = require('path')
+const sd = require('silly-datetime')
 
-const logger = morgan('tiny')
+// 服务器日志
+const logger = morgan('combined', {
+  stream: fs.createWriteStream(
+    path.join(
+      __dirname,
+      `../logs/${sd.format(new Date(), 'YYYY_MM_DD_HH_mm_ss')}.log`
+    ),
+    { flags: 'a' }
+  ),
+})
 
 class CloudBaseRunServer {
   constructor() {
