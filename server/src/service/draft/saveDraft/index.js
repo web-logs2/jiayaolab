bcc5cf6512a88bac1751c599913c4dd67a0abf23
Jia-Yao Zhao <63852433+zjy040525@users.noreply.github.com>
2023-03-17 12:56:50 +0800

    perf(draft): changed the if statements

--- server/src/service/draft/saveDraft/index.js
+++ server/src/service/draft/saveDraft/index.js
@@ -4,6 +4,21 @@ const { User, Draft } = require('../../../app')
 exports.main = async (req, res) => {
   const { draftId, title, tags, text, html, _private } = req.body
   const { email, password } = req.auth
+  const createDraft = async () => {
+    // 获取登录用户的id
+    const { id: userId } = await User.findOne({ where: { email, password } })
+    // 创建一个草稿，创建成功后返回该草稿的id
+    const { uuid: draftId } = await Draft.create({
+      title,
+      tags: tags.join('|'),
+      text,
+      html,
+      _private,
+      userId,
+    })
+    // 返回草稿id，以便后续更新
+    res.status(201).json(msg(201, { draftId }, '已保存！'))
+  }
 
   try {
     // 判断参数是否符合规则
@@ -32,42 +47,33 @@ exports.main = async (req, res) => {
       return
     }
 
-    // 判断是否有传参过来草稿id
-    if (draftId) {
-      const draft = await Draft.findOne({ where: { uuid: draftId } })
-      if (!draft) {
-        res.status(400).json(msg(400, null, '该草稿已被删除！'))
-        return
-      }
+    // 判断是否接收到草稿id
+    if (!draftId) {
+      // 草稿id不存在，创建一个
+      await createDraft()
+      return
+    }
 
-      // 如果有这个草稿id则更新该草稿
-      await Draft.update(
-        {
-          title,
-          tags: tags.join('|'),
-          text,
-          html,
-          _private,
-        },
-        { where: { uuid: draftId } }
-      )
-      res.status(201).json(msg(201, { draftId }, '已保存！'))
-    } else {
-      // 没有接收到传参过来的draftId，新建一个草稿
-      // 获取用户id
-      const { id: userId } = await User.findOne({ where: { email, password } })
-      // 创建一个草稿，返回该草稿的id
-      const { uuid: draftId } = await Draft.create({
+    // 判断数据库中是否存在该草稿
+    const findDraft = await Draft.findOne({ where: { uuid: draftId } })
+    if (!findDraft) {
+      // 草稿不存在，创建一个
+      await createDraft()
+      return
+    }
+
+    // 有草稿id，更新这个草稿的内容
+    await Draft.update(
+      {
         title,
         tags: tags.join('|'),
         text,
         html,
         _private,
-        userId,
-      })
-      // 返回草稿id，以便后续更新
-      res.status(201).json(msg(201, { draftId }, '已保存！'))
-    }
+      },
+      { where: { uuid: draftId } }
+    )
+    res.status(201).json(msg(201, { draftId }, '已保存！'))
   } catch (e) {
     console.error(e)
     res.status(400).json(msg(400, null, '服务器错误！'))
