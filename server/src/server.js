254bb36765d16c93830017ef169948f0dbfe1d96
Jia-Yao Zhao <63852433+zjy040525@users.noreply.github.com>
2023-05-16 02:26:49 +0000

    feat(valid): add user email and password verify

--- server/src/server.js
+++ server/src/server.js
@@ -26,9 +26,38 @@ class Server {
     )
     this.express.use(cors())
     this.express.use(logger)
-    // 用于加密用户密码的中间件
-    this.express.use((req, _res, next) => {
+    // 用于判断邮箱格式是否正确的中间件
+    this.express.use((req, res, next) => {
+      if (req.body.email) {
+        if (
+          !/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/.test(
+            req.body.email
+          )
+        ) {
+          res.status(400).json(result(400, null, '邮箱格式不正确！'))
+          return
+        }
+
+        if (req.body.email.length > 50) {
+          res.status(400).json(result(400, null, '邮箱长度不得大于50个字符！'))
+          return
+        }
+      }
+      next()
+    })
+    // 用于判断和加密用户密码的中间件
+    this.express.use((req, res, next) => {
       if (req.body.password) {
+        if (req.body.password.length < 8) {
+          res.status(400).json(result(400, null, '密码长度不得少于8个字符！'))
+          return
+        }
+
+        if (req.body.password.length > 32) {
+          res.status(400).json(result(400, null, '密码长度不得大于32个字符！'))
+          return
+        }
+
         req.body.password = SHA256(req.body.password).toString()
       }
       next()
